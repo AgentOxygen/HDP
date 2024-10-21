cf7416d6ad678affbc9a5a64e86228f698e3d18c
AgentOxygen <cameronscummins@gmail.com>
2024-10-21 12:47:16 -0500

    Added operator overloading and fixed bug with percentile detection

--- hdp/hw_dataset.py
+++ hdp/hw_dataset.py
@@ -12,10 +12,8 @@ import hdp.plotting_functions as hplts
 class HeatwaveDataset(xarray.Dataset):
     __slots__ = ['dataset',]
     
-    def __init__(self, dataset):
-        super(HeatwaveDataset, self).__init__(dataset, attrs=dataset.attrs)
-
     def __init__(self,
+                 dataset=None,
                  base_tmin=None,
                  base_tmax=None,
                  base_tavg=None,
@@ -28,6 +26,9 @@ class HeatwaveDataset(xarray.Dataset):
                  definitions=None,
                  client=None):
         # Implement error checking
+        if dataset is not None:
+            super(HeatwaveDataset, self).__init__(dataset, attrs=dataset.attrs)
+            return
 
         client = client or dask.distributed.client._get_global_client()
         if client is None:
@@ -70,9 +71,9 @@ class HeatwaveDataset(xarray.Dataset):
         hw_datasets = []
         for measure in measures:
             # Omitting passing path for now, may utilize a check of the encoding attribute later
-            threshold = hdp.compute_threshold(measures[measure]["base"], percentiles, None)
+            threshold = hdp.hdp.compute_threshold(measures[measure]["base"], percentiles, None)
             hw_datasets.append(
-                hdp.sample_heatwave_metrics(measures[measure]["test"], threshold["threshold"], definitions)
+                hdp.hdp.sample_heatwave_metrics(measures[measure]["test"], threshold["threshold"], definitions)
             )
         dataset = xarray.concat(
             hw_datasets,
@@ -88,17 +89,33 @@ class HeatwaveDataset(xarray.Dataset):
             return "Unknown"
         
     def plot(self):
-        if self.percentile.size == 1 and self.definition.size == 1:
-            figures = [hplts.create_single_hw_plot(self)]
-        elif self.percentile.size == 1:
-            figures = [hplts.create_single_perc_plot(self)]
-        elif self.definition.size == 1:
-            figures = [hplts.create_single_def_plot(self)]
-        else:
-            figures = [
-                hplts.create_global_mean_lineplot(self),
-                hplts.create_global_mean_implot(self)
-            ]
-        for f in figures:
+        total_figures = []
+        for measure in self.measure.values:
+            figures = []
+            ds = self.sel(measure=measure)
+            single_perc = "percentile" not in ds or ds.percentile.size == 1
+            single_def = "definition" not in ds or ds.definition.size == 1
+            
+            if single_perc and single_def:
+                figures += [hplts.create_single_hw_plot(ds)]
+            elif single_perc:
+                figures += [hplts.create_single_perc_plot(ds)]
+            elif single_def:
+                figures += [hplts.create_single_def_plot(ds)]
+            else:
+                figures += [
+                    hplts.create_global_mean_lineplot(ds),
+                    hplts.create_global_mean_implot(ds)
+                ]
+            figures.append(hplts.create_threshold_plot(ds))
+            for f in figures:
+                total_figures.append(f)
+        for f in total_figures:
             f.show()
-        return figures + [hplts.create_threshold_plot(self)]
+        return total_figures
+
+    def __sub__(self, other):
+        return HeatwaveDataset(super().__sub__(other))
+
+    def __add__(self, other):
+        return HeatwaveDataset(super().__sub__(other))
