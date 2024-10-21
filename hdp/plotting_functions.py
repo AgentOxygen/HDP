2f2df0f81a5301cd6b2b5ccbd5439038db1b64e3
AgentOxygen <cameronscummins@gmail.com>
2024-10-21 12:49:37 -0500

    Added diverging cmap switch and fixed bug with percentile detection in threshold plots

--- hdp/plotting_functions.py
+++ hdp/plotting_functions.py
@@ -12,21 +12,21 @@ plt.style.use("hdp/pacl.mplstyle")
 
 
 class WinkelTripel(ccrs._WarpedRectangularProjection):
-    """
-    Winkel-Tripel projection implementation for Cartopy
-    """
+	"""
+	Winkel-Tripel projection implementation for Cartopy
+	"""
 
-    def __init__(self, central_longitude=0.0, central_latitude=0.0, globe=None):
-        globe = globe or ccrs.Globe(semimajor_axis=ccrs.WGS84_SEMIMAJOR_AXIS)
-        proj4_params = [('proj', 'wintri'),
-                        ('lon_0', central_longitude),
-                        ('lat_0', central_latitude)]
+	def __init__(self, central_longitude=0.0, central_latitude=0.0, globe=None):
+		globe = globe or ccrs.Globe(semimajor_axis=ccrs.WGS84_SEMIMAJOR_AXIS)
+		proj4_params = [('proj', 'wintri'),
+						('lon_0', central_longitude),
+						('lat_0', central_latitude)]
 
-        super(WinkelTripel, self).__init__(proj4_params, central_longitude, globe=globe)
+		super(WinkelTripel, self).__init__(proj4_params, central_longitude, globe=globe)
 
-    @property
-    def threshold(self):
-        return 1e4
+	@property
+	def threshold(self):
+		return 1e4
 
 def create_global_mean_lineplot(hw_ds):
 	hwf_spatial_mean = hw_ds.HWF.weighted(np.cos(np.deg2rad(hw_ds.lat))).mean(dim=["lat", "lon"])
@@ -107,6 +107,8 @@ def create_global_mean_implot(hw_ds):
 	
 	ax1 = f.add_subplot(gridspec[0, 0])
 	ax2 = f.add_subplot(gridspec[0, 1])
+
+	
 	
 	im1 = hwf_spatial_mean.mean(dim="time").plot(ax=ax1, cmap="Reds")
 	im1.colorbar.set_label('Days', rotation=270, labelpad=10)
@@ -174,13 +176,21 @@ def create_single_hw_plot(hw_ds):
 	ax4 = f.add_axes(ax4.get_position(), frameon=True, projection=WinkelTripel(), zorder=-1)
 
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWF.mean(dim=["time"]).values, hw_ds.lon.values, axis=-1)
-	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax3_cbar = f.colorbar(ax3_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax3_cbar.set_label("HWF (Days)")
 	ax3.coastlines()
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWD.mean(dim=["time"]).values, hw_ds.lon.values, axis=-1)
-	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax4_cbar = f.colorbar(ax4_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax4_cbar.set_label("HWD (Days)")
 	ax4.coastlines()
@@ -244,13 +254,21 @@ def create_single_def_plot(hw_ds):
 	ax2.set_title("Global Mean HWD")
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWF.mean(dim=["time", "percentile"]).values, hw_ds.lon.values, axis=-1)
-	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax3_cbar = f.colorbar(ax3_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax3_cbar.set_label("HWF (Days)")
 	ax3.coastlines()
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWD.mean(dim=["time", "percentile"]).values, hw_ds.lon.values, axis=-1)
-	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax4_cbar = f.colorbar(ax4_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax4_cbar.set_label("HWD (Days)")
 	ax4.coastlines()
@@ -314,67 +332,79 @@ def create_single_perc_plot(hw_ds):
 	ax2.set_title("Global Mean HWD")
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWF.mean(dim=["time", "definition"]).values, hw_ds.lon.values, axis=-1)
-	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax3_contour = ax3.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax3_cbar = f.colorbar(ax3_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax3_cbar.set_label("HWF (Days)")
 	ax3.coastlines()
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.HWD.mean(dim=["time", "definition"]).values, hw_ds.lon.values, axis=-1)
-	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax4_contour = ax4.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax4_cbar = f.colorbar(ax4_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax4_cbar.set_label("HWD (Days)")
 	ax4.coastlines()
 	return f
 
 def create_multi_threshold_plot(hw_ds):
-    f = plt.figure(figsize=(8.6, 3.5), dpi=150)
-    gridspec = f.add_gridspec(1, 2)
-    
-    ax1 = f.add_subplot(gridspec[0, 0])
-    ax2 = f.add_subplot(gridspec[0, 1])
-    
-    hw_ds_spatial = hw_ds.threshold.mean(dim=["lat", "lon"])
-    
-    ax1.set_xlim(hw_ds.day[0], hw_ds.day[-1])
-    ax1.set_xlabel("Day Of Year")
-    ax1.set_ylabel(f"Threshold ({hw_ds.get_threshold_units()})")
-    ax1.set_title("Global Mean Threshold")
-    
-    cmap = plt.get_cmap('winter')
-    value = 0.5
-    norm = Normalize(vmin=hw_ds.percentile[0], vmax=hw_ds.percentile[-1])
-    
-    for perc in hw_ds.percentile:
-        normalized_value = norm(perc)
-        color = cmap(normalized_value)
-        
-        ax1.plot(
-            hw_ds_spatial.day,
-            hw_ds_spatial.sel(percentile=perc),
-            color=color
-        )
-    
-    sm = ScalarMappable(cmap=cmap, norm=norm)
-    sm.set_array([])
-    cbar1 = plt.colorbar(sm, ax=ax1)
-    cbar1.set_label('Percentile')
-    
-    ax2.set_title("Mean DOY, Perc. Std. Threshold")
-    ax2.xaxis.set_visible(False)
-    ax2.yaxis.set_visible(False)
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-    ax2.spines['left'].set_visible(False)
-    ax2.spines['bottom'].set_visible(False)
-    ax2.grid(False)
-    ax2 = f.add_axes(ax2.get_position(), frameon=True, projection=WinkelTripel(), zorder=-1)
-    
-    cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.threshold.mean(dim="day").std(dim="percentile").values, hw_ds.lon.values, axis=-1)
-    ax2_contour = ax2.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
-    ax2_cbar = f.colorbar(ax2_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
-    ax2_cbar.set_label(f"Threshold ({hw_ds.get_threshold_units()})")
-    ax2.coastlines()
-    return f
+	f = plt.figure(figsize=(8.6, 3.5), dpi=150)
+	gridspec = f.add_gridspec(1, 2)
+	
+	ax1 = f.add_subplot(gridspec[0, 0])
+	ax2 = f.add_subplot(gridspec[0, 1])
+	
+	hw_ds_spatial = hw_ds.threshold.mean(dim=["lat", "lon"])
+	
+	ax1.set_xlim(hw_ds.day[0], hw_ds.day[-1])
+	ax1.set_xlabel("Day Of Year")
+	ax1.set_ylabel(f"Threshold ({hw_ds.get_threshold_units()})")
+	ax1.set_title("Global Mean Threshold")
+	
+	cmap = plt.get_cmap('winter')
+	value = 0.5
+	norm = Normalize(vmin=hw_ds.percentile[0], vmax=hw_ds.percentile[-1])
+	
+	for perc in hw_ds.percentile:
+		normalized_value = norm(perc)
+		color = cmap(normalized_value)
+		
+		ax1.plot(
+			hw_ds_spatial.day,
+			hw_ds_spatial.sel(percentile=perc),
+			color=color
+		)
+	
+	sm = ScalarMappable(cmap=cmap, norm=norm)
+	sm.set_array([])
+	cbar1 = plt.colorbar(sm, ax=ax1)
+	cbar1.set_label('Percentile')
+	
+	ax2.set_title("Mean DOY, Perc. Std. Threshold")
+	ax2.xaxis.set_visible(False)
+	ax2.yaxis.set_visible(False)
+	ax2.spines['top'].set_visible(False)
+	ax2.spines['right'].set_visible(False)
+	ax2.spines['left'].set_visible(False)
+	ax2.spines['bottom'].set_visible(False)
+	ax2.grid(False)
+	ax2 = f.add_axes(ax2.get_position(), frameon=True, projection=WinkelTripel(), zorder=-1)
+	
+	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.threshold.mean(dim="day").std(dim="percentile").values, hw_ds.lon.values, axis=-1)
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax2_contour = ax2.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
+	ax2_cbar = f.colorbar(ax2_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
+	ax2_cbar.set_label(f"Threshold ({hw_ds.get_threshold_units()})")
+	ax2.coastlines()
+	return f
 
 def create_single_threshold_plot(hw_ds):
 	f = plt.figure(figsize=(8.6, 3.5), dpi=150)
@@ -400,14 +430,18 @@ def create_single_threshold_plot(hw_ds):
 	ax2 = f.add_axes(ax2.get_position(), frameon=True, projection=WinkelTripel(), zorder=-1)
 	
 	cyclic_values, cyclic_lons = add_cyclic_point(hw_ds.threshold.mean(dim=["day"]).values, hw_ds.lon.values, axis=-1)
-	ax2_contour = ax2.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap="Reds")
+	if np.min(cyclic_values) < 0:
+		cmap="bwr"
+	else:
+		cmap="Reds"
+	ax2_contour = ax2.contourf(cyclic_lons, hw_ds.lat.values, cyclic_values, transform=ccrs.PlateCarree(), cmap=cmap)
 	ax2_cbar = f.colorbar(ax2_contour, location="bottom", anchor=(0.0, 0.0), pad=0)
 	ax2_cbar.set_label(f"Threshold ({hw_ds.get_threshold_units()})")
 	ax2.coastlines()
 	return f
 
 def create_threshold_plot(hw_ds):
-    if hw_ds.percentile.size > 1:
-        return create_multi_threshold_plot(hw_ds)
-    else:
-        return create_single_threshold_plot(hw_ds)
+	if "percentile" in hw_ds and hw_ds.percentile.size > 1:
+		return create_multi_threshold_plot(hw_ds)
+	else:
+		return create_single_threshold_plot(hw_ds)
