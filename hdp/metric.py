613b1cf04c2f73efd043740180c172cb66373c1b
AgentOxygen <cameron.cummins@utexas.edu>
2025-01-09 16:01:13 -0600

    Added documentation and simplified doy map

--- hdp/metric.py
+++ hdp/metric.py
@@ -9,11 +9,16 @@ def index_heatwaves(hot_days_ts: np.ndarray, min_duration: int, max_break: int, 
     """
     Identifies the heatwaves in the timeseries using the specified heatwave definition
 
-    Keyword arguments:
-    timeseries -- integer array of ones and zeros where ones indicates a hot day (numpy.ndarray)
-    max_break -- the maximum number of days between hot days within one heatwave event (default 1)
-    min_duration -- the minimum number of hot days to constitute a heatwave event, including after breaks (default 3)
-    max_subs -- the maximum number of subsequent events allowed to be apart of the initial consecutive hot days
+    :param hot_days_ts: Integer array of ones and zeros where ones indicates a hot day
+    :type hot_days_ts: np.ndarray
+    :param min_duration: The minimum number of hot days to constitute a heatwave event, including after breaks
+    :type min_duration: int
+    :param max_break: The maximum number of days between hot days within one heatwave event
+    :type max_break: int
+    :param max_subs: the maximum number of subsequent events allowed to be apart of the initial consecutive hot days
+    :type max_subs: int
+    :return: Timeseries where nonzero integers indicate heatwave indices for each timestep
+    :rtype: np.ndarray
     """
     ts = np.zeros(hot_days_ts.size + 2, dtype=nb.int64)
     for i in range(0, hot_days_ts.size):
@@ -53,6 +58,16 @@ def index_heatwaves(hot_days_ts: np.ndarray, min_duration: int, max_break: int, 
 
 @nb.njit
 def heatwave_number(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarray:
+    """
+    Summary
+
+    :param hw_ts: Integer timeseries of indexed heatwave days.
+    :type hw_ts: np.ndarray
+    :param season_ranges: Range of array indices, corresponding to heatwave season, in indexed heatwave day timeseries to count.
+    :type season_ranges: np.ndarray
+    :return: Number of heatwaves per heatwave season
+    :rtype: np.ndarray
+    """
     output = np.zeros(season_ranges.shape[0], dtype=nb.int64)
     for y in range(season_ranges.shape[0]):
         end_points = season_ranges[y]
@@ -62,6 +77,16 @@ def heatwave_number(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarray:
 
 @nb.njit
 def heatwave_frequency(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarray:
+    """
+    Summary
+
+    :param hw_ts: Integer timeseries of indexed heatwave days.
+    :type hw_ts: np.ndarray
+    :param season_ranges: Range of array indices, corresponding to heatwave season, in indexed heatwave day timeseries to count.
+    :type season_ranges: np.ndarray
+    :return: Number of heatwave days per heatwave season.
+    :rtype: np.ndarray
+    """
     output = np.zeros(season_ranges.shape[0], dtype=nb.int64)
     for y in range(season_ranges.shape[0]):
         end_points = season_ranges[y]
@@ -71,6 +96,16 @@ def heatwave_frequency(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarr
 
 @nb.njit
 def heatwave_duration(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarray:
+    """
+    Summary
+
+    :param hw_ts: Integer timeseries of indexed heatwave days.
+    :type hw_ts: np.ndarray
+    :param season_ranges: Range of array indices, corresponding to heatwave season, in indexed heatwave day timeseries to count.
+    :type season_ranges: np.ndarray
+    :return: Length of longest heatwave per heatwave season.
+    :rtype: np.ndarray
+    """
     output = np.zeros(season_ranges.shape[0], dtype=nb.int64)
     for y in range(season_ranges.shape[0]):
         end_points = season_ranges[y]
@@ -86,7 +121,19 @@ def heatwave_duration(hw_ts: np.ndarray, season_ranges: np.ndarray) -> np.ndarra
     return output
 
 
-def get_range_indices(times: np.array, start: tuple, end: tuple):
+def get_range_indices(times: np.ndarray, start: tuple, end: tuple) -> np.ndarray:
+    """
+    Summary
+
+    :param times: Array of CFTime objects.
+    :type times: np.ndarray
+    :param start: Tuple of starting month integer and day integer to compare times array against.
+    :type start: tuple
+    :param end: Tuple of ending month integer and day integer to compare times array against.
+    :type end: tuple
+    :return: Range of indices in times array for each season.
+    :rtype: np.ndarray
+    """
     num_years = times[-1].year - times[0].year + 1
     ranges = np.zeros((num_years, 2), dtype=int) - 1
 
@@ -109,7 +156,15 @@ def get_range_indices(times: np.array, start: tuple, end: tuple):
     return ranges
 
 
-def compute_hemisphere_ranges(measure: xarray.DataArray):
+def compute_hemisphere_ranges(measure: xarray.DataArray) -> xarray.DataArray:
+    """
+    Summary
+
+    :param measure: DataArray containing 'lat' and 'lon' variables corresponding to grid.
+    :type measure: xarray.DataArray
+    :return: Generates summer seasonal ranges by hemisphere for an arbitrary 'lat'-'lon' grid.
+    :rtype: xarray.DataArray
+    """
     north_ranges = get_range_indices(measure.time.values, (5, 1), (10, 1))
     south_ranges = get_range_indices(measure.time.values, (10, 1), (3, 1))
 
@@ -132,19 +187,39 @@ def compute_hemisphere_ranges(measure: xarray.DataArray):
                             })
 
 
-def build_doy_map(temperatures: xarray.DataArray, threshold: xarray.DataArray):
-    doy_map = np.zeros(temperatures.time.size, dtype=int) - 1
-    for time_index, time in enumerate(temperatures.time.values):
+def build_doy_map(times: np.ndarray) -> np.ndarray:
+    """
+    Summary
+
+    :param measure: Array of CFTime objects.
+    :type measure: np.ndarray
+    :return: Day of year for each timestep.
+    :rtype: np.ndarray
+    """
+    doy_map = np.zeros(measure.time.size, dtype=int) - 1
+    for time_index, time in enumerate(times.values):
         doy_map[time_index] = time.dayofyr - 1
     return doy_map
 
 
 @nb.njit
-def indicate_hot_days(temperatures: np.ndarray, threshold: np.ndarray, doy_map: np.ndarray):
-    output = np.zeros(temperatures.shape, dtype=nb.boolean)
-    for t in range(temperatures.size):
+def indicate_hot_days(measure: np.ndarray, threshold: np.ndarray, doy_map: np.ndarray) -> np.ndarray:
+    """
+    Summary
+
+    :param measure: Heat measure to compare against the threshold.
+    :type measure: np.ndarray
+    :param threshold: Threshold for extreme heat.
+    :type threshold: np.ndarray
+    :param doy_map: Mapping from index in times array to day of year value.
+    :type doy_map: np.ndarray
+    :return: Boolean array of days in measure that exceed the threshold.
+    :rtype: np.ndarray
+    """
+    output = np.zeros(measure.shape, dtype=nb.boolean)
+    for t in range(measure.size):
         doy = doy_map[t]
-        if temperatures[t] > threshold[doy]:
+        if measure[t] > threshold[doy]:
             output[t] = True
         else:
             output[t] = False
@@ -152,10 +227,30 @@ def indicate_hot_days(temperatures: np.ndarray, threshold: np.ndarray, doy_map: 
 
 
 @nb.njit
-def compute_heatwave_metrics(temperatures: np.ndarray, threshold: np.ndarray, doy_map: np.ndarray,
+def compute_heatwave_metrics(measure: np.ndarray, threshold: np.ndarray, doy_map: np.ndarray,
                              min_duration: int, max_break: int, max_subs: int,
                              season_ranges: np.ndarray) -> np.ndarray:
-    hot_days_ts = indicate_hot_days(temperatures, threshold, doy_map)
+    """
+    Summary
+
+    :param measure: Heat measure to compare against the threshold.
+    :type measure: np.ndarray
+    :param threshold: Threshold for extreme heat.
+    :type threshold: np.ndarray
+    :param doy_map: Mapping from index in times array to day of year value.
+    :type doy_map: np.ndarray
+    :param min_duration: Minimum number of days that exceed the threshold to constitute a heatwave.
+    :type min_duration: int
+    :param max_break: Maximum number of days below the threshold that can exist in a heatwave.
+    :type max_break: int
+    :param max_subs: Maximum number of subsequent heatwaves.
+    :type max_subs: int
+    :param season_ranges: 
+    :type season_ranges: np.ndarray
+    :return: Array of equal number of dimensions to measure and an additional dimension containing each heatwave metric: HWF, HWD, HWN
+    :rtype: np.ndarray
+    """
+    hot_days_ts = indicate_hot_days(measure, threshold, doy_map)
     hw_ts = index_heatwaves(hot_days_ts, min_duration, max_break, max_subs)
     hwf = heatwave_frequency(hw_ts, season_ranges)
     hwd = heatwave_duration(hw_ts, season_ranges)
@@ -167,7 +262,23 @@ def compute_heatwave_metrics(temperatures: np.ndarray, threshold: np.ndarray, do
     return output
 
 
-def compute_metrics(measure: xarray.DataArray, threshold: xarray.DataArray, hw_definitions: list, include_threshold: bool=True, check_variables: bool=True):
+def compute_metrics(measure: xarray.DataArray, threshold: xarray.DataArray, hw_definitions: list, include_threshold: bool=True, check_variables: bool=True) -> xarray.Dataset:
+    """
+    Summary
+
+    :param measure: 
+    :type measure: xarray.DataArray
+    :param threshold: 
+    :type threshold: xarray.DataArray
+    :param hw_definitions: 
+    :type hw_definitions: list
+    :param include_threshold: 
+    :type include_threshold: bool
+    :param check_variables: 
+    :type check_variables: bool
+    :return: 
+    :rtype: xarray.Dataset
+    """
     if check_variables:
         assert threshold.name == "threshold_" + measure.name
         assert "hdp_type" in threshold.attrs
@@ -190,7 +301,7 @@ def compute_metrics(measure: xarray.DataArray, threshold: xarray.DataArray, hw_d
                                          })
         
         doy_map = xarray.DataArray(
-            data=build_doy_map(measure, perc_threshold),
+            data=build_doy_map(times),
             coords={"time": times}
         )
         
@@ -262,6 +373,26 @@ def compute_metrics_io(output_path: str,
                        hw_definitions: list,
                        include_threshold: bool=False,
                        override_threshold_var: str=None):
+    """
+    Summary
+
+    :param output_path: 
+    :type output_path: str
+    :param measure_path: 
+    :type measure_path: str
+    :param measure_var: 
+    :type measure_var: str
+    :param threshold_path: 
+    :type threshold_path: str
+    :param hw_definitions: 
+    :type hw_definitions: list
+    :param include_threshold: 
+    :type include_threshold: bool
+    :param override_threshold_var: 
+    :type override_threshold_var: str
+    :return: 
+    :rtype: None
+    """
     output_path = Path(output_path)
     measure_path = Path(measure_path)
     threshold_path = Path(threshold_path)
