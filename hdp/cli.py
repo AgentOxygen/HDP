adbc30b2f581271a24e545b0ea73f652d1747555
AgentOxygen <cameron.cummins@utexas.edu>
2025-01-09 16:00:24 -0600

    Added version parameter

--- hdp/cli.py
+++ hdp/cli.py
@@ -9,14 +9,21 @@ Contact: cameron.cummins@utexas.edu
 """
 from importlib.metadata import version as getVersion
 import argparse
-
+from hdp.threshold import compute_threshold_io
+from hdp.metric import compute_metrics_io
 
 def main():
     parser = argparse.ArgumentParser(description = f"Command Line Interface (CLI) for Heatwave Diagnostics Package (HDP) Version {getVersion('hdp_python')}")
     subparsers = parser.add_subparsers(help='HDP commands')
     parser.add_argument('-d', '--dask', metavar="ADDRESS", type=str, help="Address of existing dask cluster to connect to instead of creating internally")
     parser.add_argument('-o', '--overwrite', action='store_true')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-v', '--version', action='store_true')
+    parser.add_argument('-V', '--verbose', action='store_true')
+
+    args = parser.parse_args()
+    if args.version:
+        print(getVersion('hdp_python'))
+        return None
     
     threshold_parser = subparsers.add_parser('threshold', help='Generate a range of extreme heat thresholds from baseline heat measure datasets')
     metric_parser = subparsers.add_parser('metric', help='Generate heatwave metrics for given threshold and heat measure datasets')
@@ -36,9 +43,26 @@ def main():
     metric_parser.add_argument('-var', '--variable', nargs='+', type=str, help="Variable(s) to use from heat measure dataset (defaults to all available)")
     metric_parser.add_argument('-p','--percentile', nargs='+', help='List of percentiles to calculate metrics for, assuming thresholds exist (ex: 0.9 0.91 0.92)')    
     
-    args = parser.parse_args()
     threshold_args = threshold_parser.parse_args()
     metric_args = metric_parser.parse_args()
+    
+    # compute_threshold_io(baseline_path: str,
+    #                      baseline_var: str,
+    #                      output_path: str,
+    #                      percentiles: np.ndarray,
+    #                      no_season: bool=False,
+    #                      rolling_window_size: int=7,
+    #                      fixed_value: float=None,
+    #                      overwrite: bool=False)
+
+    # compute_metrics_io(output_path: str,
+    #                    measure_path: str,
+    #                    measure_var: str,
+    #                    threshold_path: str,
+    #                    hw_definitions: list,
+    #                    include_threshold: bool=False,
+    #                    override_threshold_var: str=None)
+
 
 if __name__ == "__main__":
     main()
