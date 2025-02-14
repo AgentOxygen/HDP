8ca88633bd767d5e8a4a187a4017f91934794e4a
AgentOxygen <cameron.cummins@utexas.edu>
2025-02-14 11:02:17 -0600

    Changed docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -33,5 +33,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.ipynb_checkpoints']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
