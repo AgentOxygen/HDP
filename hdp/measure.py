225e41ffeacb48ab831f96fd2f50608d2ecd32b4
AgentOxygen <cameron.cummins@utexas.edu>
2025-02-17 17:30:48 -0600

    Fixed bug in heat_index function with extreme values
    Need to implement extreme humidity handling for super-saturation cases (the regression is not accurate above 100%)

--- hdp/measure.py
+++ hdp/measure.py
@@ -65,7 +65,7 @@ def heat_index(temp: float, rel_humid: float) -> float:
 
     This function relies on a regression of the National Weather Service heat index.
     It has an error of +/- 1.3 degrees Fahrenheit.
-    Source: https://www.weather.gov/ama/heatindex
+    https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
 
     :param temp: Temperature value in degrees Fahrenheit
     :type temp: float
@@ -74,24 +74,23 @@ def heat_index(temp: float, rel_humid: float) -> float:
     :return: Heat index value in degrees Fahrenheit
     :rtype: float
     """
-    hi = -42.379
-    hi += 2.04901523*temp
-    hi += 10.14333127*rel_humid 
-    hi += -0.22475541*temp*rel_humid 
-    hi += -0.00683783*(temp**2)
-    hi += -0.05481717*(rel_humid**2)
-    hi += 0.00122874*(temp**2)*rel_humid
-    hi += 0.00085282*temp*(rel_humid**2)
-    hi += -0.00000199*((rel_humid*temp)**2)
-    
-    if rel_humid < 13 and 80 <= temp <= 112:
-        hi -= ((13 - rel_humid)/4)*np.sqrt(((17 - np.abs(temp - 95))/17))
-    elif rel_humid > 85 and 80 <= temp <= 87:
-        hi += ((rel_humid - 85)/10) * ((87 - temp)/5)
-    
-    if hi < 80:
-        hi = 0.5 * (temp + 61.0 + ((temp - 68.0)*1.2) + (rel_humid*0.094))
-
+    hi = 0.5 * (temp + 61.0 + ((temp - 68.0)*1.2) + (rel_humid*0.094))
+    if hi > 80:
+        hi = -42.379
+        hi += 2.04901523*temp
+        hi += 10.14333127*rel_humid 
+        hi += -0.22475541*temp*rel_humid 
+        hi += -0.00683783*(temp**2)
+        hi += -0.05481717*(rel_humid**2)
+        hi += 0.00122874*(temp**2)*rel_humid
+        hi += 0.00085282*temp*(rel_humid**2)
+        hi += -0.00000199*((rel_humid*temp)**2)
+        
+        if rel_humid < 13 and 80 <= temp <= 112:
+            hi -= ((13 - rel_humid)/4)*np.sqrt(((17 - np.abs(temp - 95))/17))
+        elif rel_humid > 85 and 80 <= temp <= 87:
+            hi += ((rel_humid - 85)/10) * ((87 - temp)/5)
+            
     return hi
 
 
