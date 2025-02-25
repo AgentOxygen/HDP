c5d2b66dfbde64296aff124615e5a61762a6892d
AgentOxygen <cameron.cummins@utexas.edu>
2025-02-25 11:29:15 -0600

    Added synthetic dataset functions for unit testing

--- hdp/utils.py
+++ hdp/utils.py
@@ -1,7 +1,11 @@
-import xarray
-import datetime
 from time import time
 from importlib.metadata import version
+import numpy as np
+import dask.array as da
+import xarray
+import datetime
+import cftime
+
 
 def get_time_stamp():
     return datetime.datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M')
@@ -30,3 +34,46 @@ def get_func_description(func):
         if line != "":
             desc += line.strip() + " "
     return desc
+
+
+def generate_synthetic_dataset(center=25, amplitude=10, name="temperature", units="degC"):
+    time_values = xarray.cftime_range(
+        start=cftime.DatetimeNoLeap(2000, 1, 1),
+        end=cftime.DatetimeNoLeap(2009, 12, 31),
+        freq="D",
+        calendar="noleap"
+    )
+    
+    temp_timeseries = center + amplitude*np.sin(np.pi*np.arange(time_values.size, dtype=float) / 365)
+    temp_values = np.broadcast_to(temp_timeseries, (3, 3, temp_timeseries.size))
+    
+    temp_da = xarray.DataArray(
+        data=da.from_array(temp_values),
+        dims=["lat", "lon", "time"],
+        coords={
+            "lat": np.array([-90, 0, 90], dtype=float),
+            "lon": np.array([-180, 0, 180], dtype=float),
+            "time": time_values
+        },
+        name=name,
+        attrs={
+            "units": units
+        }
+    ).chunk(dict(lat=1, lon=1))
+    return xarray.Dataset({name: temp_da})
+
+
+def generate_exceedance_dataarray(measure, exceedance_pattern, multiplier=1.0):
+    tiles = np.ceil(measure.time.size / len(exceedance_pattern)).astype(int)
+    pattern_data = np.broadcast_to(np.tile(exceedance_pattern, tiles)[:measure.time.size], measure.shape)*multiplier
+
+    ret_da = None
+    with xarray.set_options(keep_attrs=True): 
+        ret_da = measure + xarray.DataArray(
+            data=da.from_array(pattern_data),
+            dims=measure.dims,
+            coords=measure.coords,
+            name=measure.name,
+            attrs=measure.attrs
+        ).chunk(measure.chunksizes)
+    return ret_da
